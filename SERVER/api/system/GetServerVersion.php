c51aa78b6bb7802075603900ff08eff965c3084a
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-16 19:55:15 +0100

    GET API najnowszej wersji CMS - po stronie serwera

---
+++ SERVER/api/system/GetServerVersion.php
@@ -0,0 +1,27 @@
+<?php
+
+header('Content-Type: application/json');
+
+// RODZAJE WERSJI:
+// alpha - niedokończona, wiele błędów. Testowana przez ogr. grupę
+// beta - po alphie, bardziej stabilna, nadal błędy. Testowana publicznie
+
+// release candidate (rc) - potencjalnie ostateczna. Jeżeli nie ma błędów -> sable
+// stable - ostateczna wersja
+// LUB
+// pre-release
+// release
+
+// RODZAJE WERSJI (numerki):
+// major.minor.patch
+// major - znaczne zmiany (nowe funkcje, interfejsy)
+// minor - mniejsze aktual., poprawki i drobne ulepszenia
+// patch - małe akt., naprawiają konkretne bugi/lepsze zabezpiecznia
+
+// "dev-1.0.0"
+
+require_once("Version.inc.php");
+
+if(isset($_POST["complex"]) && $_POST["complex"]) $complex = true;
+else $complex = false;
+die(Version::getVersion("server",$complex));
