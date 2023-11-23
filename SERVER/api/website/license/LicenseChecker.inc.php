d5501b801ff79070920ac3793c1aa266d5d0dcc7
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-23 23:12:58 +0100

    Poprawka czytelności kodu, dodanie weryfikacji, czy licencja nie wygasła

--- SERVER/api/website/license/LicenseChecker.inc.php
+++ SERVER/api/website/license/LicenseChecker.inc.php
@@ -1,90 +1,122 @@
 <?php
 
-    header("Content-Type: application/json; charset=utf-8");
+header("Content-Type: application/json; charset=utf-8");
 
-    require_once(dirname(__DIR__, 3) . "/objects/Website.inc.php");
-    function getTries($ip): int
-    {
-        $conn = Connection::getConnection();
+require_once(dirname(__DIR__, 3) . "/objects/Website.inc.php");
+function getTries($ip): int
+{
+    $conn = Connection::getConnection();
 
-        date_default_timezone_set("Europe/Warsaw");
-        $date = date("Y-m-d");
+    date_default_timezone_set("Europe/Warsaw");
+    $date = date("Y-m-d");
 
-        $result = $conn->query("SELECT date FROM check_license_history WHERE ip='$ip' AND date LIKE '$date%' AND successful=0");
-        $res = 5 - $result->num_rows;
-        $conn->close();
-        return $res;
-    }
+    $result = $conn->query("SELECT date FROM check_license_history WHERE ip='$ip' AND date LIKE '$date%' AND successful=0");
+    $res = 5 - $result->num_rows;
+    $conn->close();
+    return $res;
+}
 
-    function isBanned($ip): bool
-    {
-        if(getTries($ip) <= 0) return true;
+function isBanned($ip): bool
+{
+    if(getTries($ip) <= 0) return true;
 
-        $conn = Connection::getConnection();
-        $result = $conn->query("SELECT * FROM check_license_banned_ips WHERE ip='$ip'");
-        $banned = $result->num_rows >= 1;
+    $conn = Connection::getConnection();
+    $result = $conn->query("SELECT * FROM check_license_banned_ips WHERE ip='$ip'");
+    $banned = $result->num_rows >= 1;
 
-        $conn->close();
-        
-        if(!$banned) $banned = getTries($ip) <= 0;
-        return $banned;
-    }
+    $conn->close();
 
-    function addCheckLicenseHistory($ip, $requestDomain, $domain, $login, $licenseKey, $successful): void
-    {
-        $conn = Connection::getConnection();
-        if(!$successful) $successful = "0";
-        $conn->query("INSERT INTO check_license_history (ip,request_domain,domain,login,license_key,successful) VALUES ('$ip','$requestDomain','$domain','$login','$licenseKey',$successful)");
-        $conn->close();
-    }
+    if(!$banned) $banned = getTries($ip) <= 0;
+    return $banned;
+}
 
-    function checkLicense($remoteAddr, $httpReferer, $domain, $login, $license_key): array
-    {
-        if(isBanned($remoteAddr)) return ["err" => "To IP jest zablokowane!"];
+function addCheckLicenseHistory($ip, $requestDomain, $domain, $login, $licenseKey, $successful, $description): void
+{
+    $conn = Connection::getConnection();
+    if(!$successful) $successful = "0";
+    date_default_timezone_set('Europe/Warsaw');
+    $now = date("Y-m-d H:i:s");
+    $conn->query("INSERT INTO check_license_history (ip,request_domain,domain,login,license_key,date,successful,description) VALUES ('$ip','$requestDomain','$domain','$login','$licenseKey','$now',$successful,'$description')");
+    $conn->close();
+}
 
-        $requestDomain = null;
-        $referer = parse_url($httpReferer);
-        if(isset($referer["host"]))
-            $requestDomain = $referer["host"];
+function checkLicense($remoteAddr, $httpReferer, $domain, $login, $license_key): array
+{
+    if (isBanned($remoteAddr)) return ["suc" => 0, "desc" => "To IP jest zablokowane!"];
 
-        $clientServerIps = gethostbynamel($domain);
+    $requestDomain = null;
+    $referer = parse_url($httpReferer);
+    if (isset($referer["host"]))
+        $requestDomain = $referer["host"];
+
+    $clientServerIps = gethostbynamel($domain);
 
 //        Tutaj jest jak podana domena (przez klienta i u nas) nie istnieje 😲 Jak to możliwe? Może się nigdy nie zdarzy :p
-        if($clientServerIps === false)
-        {
-            addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,false);
-            return["err" => "Nieprawidłowa nazwa hosta."];
-        }
+    if ($clientServerIps === false) {
+        addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "Nieprawidłowa nazwa hosta.");
+        return ["suc" => 0, "desc" => "Nieprawidłowa nazwa hosta."];
+    }
 
 //        TODO do wywalenia
-        $clientServerIps[] = "::1";
+    $clientServerIps[] = "::1";
 
-        if(!in_array($remoteAddr, $clientServerIps))
-        {
-//            TODO do logów sk..syna XD
-//            nie ma nic za darmo, niech płaci
-            addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,false);
-//            return["err" => "Nieprawidłowa nazwa hosta. Czy na pewno masz pliki na odpowiednim serwerze?"];
-            return["err" => "Nieprawidłowa nazwa hosta. Czy na pewno masz pliki na odpowiednim serwerze? DEBUG: TwojeIP:".$remoteAddr.";ZnalezioneIP:".join(",",$clientServerIps)];
-        }
-
-        $websiteID = Website::getWebsiteIDByMatching("domain",$domain);
-        if($websiteID == null) {
-            addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,false);
-            return(["err" => "Authorization failed.","tries_left" => getTries($remoteAddr)]);
-        }
-
-        $website = new Website($websiteID);
-        if($website->getLogin() != $login) {
-            addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,false);
-            return(["err" => "Authorization failed.","tries_left" => getTries($remoteAddr)]);
-        }
-
-        if($website->getLicenseKey() != $license_key) {
-            addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,false);
-            return(["err" => "Authorization failed.","tries_left" => getTries($remoteAddr)]);
-        }
-
-        addCheckLicenseHistory($remoteAddr,$requestDomain,$domain,$login,$license_key,true);
-        return ["suc" => "Authorization succeeded!", "expiry_date" => $website->getLicenseExpiration()];
+    if (!in_array($remoteAddr, $clientServerIps)) {
+//        do logów sk..syna XD
+//        nie ma nic za darmo, niech płaci
+        addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "SCAM? Nieprawidłowa nazwa hosta. Czy na pewno masz pliki na odpowiednim serwerze?");
+        return ["suc" => 0, "desc" => "Nieprawidłowa nazwa hosta. Czy na pewno masz pliki na odpowiednim serwerze?"];
+//        return["suc" => 0, "desc" => "Nieprawidłowa nazwa hosta. Czy na pewno masz pliki na odpowiednim serwerze? DEBUG: TwojeIP:".$remoteAddr.";ZnalezioneIP:".join(",",$clientServerIps)];
     }
+
+    $websiteID = Website::getWebsiteIDByMatching("domain", $domain);
+    if ($websiteID == null) {
+        addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "Nie ma takiej domeny");
+        return (["suc" => 0, "desc" => "Autoryzacja nie powiodła się.", "tries_left" => getTries($remoteAddr)]);
+    }
+
+    $website = new Website($websiteID);
+    if ($website->getLogin() != $login) {
+        addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "Niepoprawny login");
+        return (["suc" => 0, "desc" => "Autoryzacja nie powiodła się.", "tries_left" => getTries($remoteAddr)]);
+    }
+
+    if ($website->getLicenseKey() != $license_key) {
+        addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "Niepoprawny klucz");
+        return (["suc" => 0, "desc" => "Autoryzacja nie powiodła się.", "tries_left" => getTries($remoteAddr)]);
+    }
+
+    $licenseExpiration = $website->getLicenseExpiration();
+
+    if ($licenseExpiration != null) {
+        $licenseDatetime = new DateTime($licenseExpiration);
+
+        date_default_timezone_set('Europe/Warsaw');
+        $nowDatetime = new DateTime(date("Y-m-d H:i:s"));
+
+        $expired = false;
+        if ($licenseDatetime->format("Y") < $nowDatetime->format("Y"))
+            $expired = true;
+        else if ($licenseDatetime->format("m") < $nowDatetime->format("m"))
+            $expired = true;
+        else if ($licenseDatetime->format("d") < $nowDatetime->format("d"))
+            $expired = true;
+
+
+        if ($expired) {
+            addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, false, "Licencja wygasła");
+            return (["suc" => 0, "desc" => "Autoryzacja nie powiodła się. Licencja wygasła!", "tries_left" => getTries($remoteAddr)]);
+        }
+
+        //    $year = $dateTime->format('Y');
+        //    $month = $dateTime->format('m');
+        //    $day = $dateTime->format('d');
+        //    $hour = $dateTime->format('H');
+        //    $minute = $dateTime->format('i');
+        //    $second = $dateTime->format('s');
+
+
+    }
+
+    addCheckLicenseHistory($remoteAddr, $requestDomain, $domain, $login, $license_key, true, "");
+    return ["suc" => 1, "desc" => "Autoryzacja powiodła się!", "expiry_date" => $licenseExpiration];
+}
