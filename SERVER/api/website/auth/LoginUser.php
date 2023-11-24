6858d9a2012ddc1176c3c9f45d25c6cb34e84ea8
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-24 23:21:08 +0100

    Aktualizacja API - dostosowanie do funkcji validatePost Dodanie nowego pola zwracającego: auth_key

--- SERVER/api/website/auth/LoginUser.php
+++ SERVER/api/website/auth/LoginUser.php
@@ -11,8 +11,9 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
 require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
 
-$loginUser = $website->loginUser($_POST["username"], $_POST["password"]);
+$loginUser = $website->loginUser($_SERVER["REMOTE_ADDR"],$_POST["username"], $_POST["password"]);
 $response["resp"] = $loginUser["suc"];
 $response["desc"] = $loginUser["desc"];
+if(isset($loginUser["auth_key"])) $response["auth_key"] = $loginUser["auth_key"];
 
 die(json_encode($response,JSON_UNESCAPED_UNICODE));
