e4e514d7005f277c916db3e9489ce3f38811163f
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-26 14:53:04 +0100

    Zmiana nazwy bazy danych

--- SERVER/objects/website/SecureKey.inc.php
+++ SERVER/objects/website/SecureKey.inc.php
@@ -9,9 +9,9 @@ class SecureKey
 
         do {
             $secureKey = bin2hex(random_bytes(64));
-        } while($conn->query("SELECT id FROM websites_keys WHERE website_id = $website_id AND secure_key = '$secureKey'")->num_rows > 0);
+        } while($conn->query("SELECT id FROM websites_secure_keys WHERE website_id = $website_id AND secure_key = '$secureKey' AND used_time IS NULL")->num_rows > 0);
 
-        $conn->query("INSERT INTO websites_keys (website_id, secure_key) VALUES ($website_id, '$secureKey')");
+        $conn->query("INSERT INTO websites_secure_keys (website_id, secure_key) VALUES ($website_id, '$secureKey')");
         $conn->close();
 
         return $secureKey;
@@ -24,7 +24,7 @@ class SecureKey
 //        TODO ??? dołożyć do tego jeszcze kolumnę `data` - gdy zostanie dodana do DB (o ile zostanie dodana)
         date_default_timezone_set('Europe/Warsaw');
         $now = date("Y-m-d H:i:s");
-        $conn->query("UPDATE websites_keys SET used_time = '$now', api_name = '$apiName' WHERE website_id = $website_id AND secure_key = '$secureKey'");
+        $conn->query("UPDATE websites_secure_keys SET used_time = '$now', api_name = '$apiName' WHERE website_id = $website_id AND secure_key = '$secureKey'");
         $conn->close();
     }
 }
