06c24933c8b978d926088f70341c9b68fe4d1afd
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-28 20:34:08 +0100

    Dodanie skryptu zabezpieczającego przed nieautoryzowanym dostępem

--- PQCMS/panel/logs/index.php
+++ PQCMS/panel/logs/index.php
@@ -1,5 +1,13 @@
+<?php
+session_start();
+if(empty($_SESSION["pqcms-panel-username"]))
+{
+    header("location: ../");
+    die("Najpierw musisz się zalogować! Błędne przekierowanie.");
+}
+?>
 <ul>
-    <li>Strona (tekst) 
+    <li>Strona (tekst)
         <!-- <pre>User;ID;Nazwa;(grupa);Stary Tekst;Nowy Tekst</pre> -->
     </li>
     <li>
@@ -15,6 +23,4 @@
     <li>
         Data (data.json, settings.json [zmiany]):
     </li>
-
 </ul>
-    
