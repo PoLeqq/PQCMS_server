1a640fcd2a7e6c7767c50fc2e5d7d96988d47431
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-29 22:58:16 +0100

    Poprawka electrocms -> pqcms

--- SERVER/client/account/logout.php
+++ SERVER/client/account/logout.php
@@ -1,24 +1,29 @@
 <?php
 
 session_start();
+
+if(empty($_SESSION["pqcms-client-logged"]))
+{
+    header("location: ../");
+    die("Nie jesteś jeszcze zalogowany! Błędne przekierowanie.");
+}
+
 //session_destroy();
 require_once(dirname(__DIR__, 2) . "/objects/Website.inc.php");
 echo "<pre>";
-var_dump(Website::getWebsiteIDByMatching("domain",$_SESSION["electrocms-client-domain"]));
+var_dump(Website::getWebsiteIDByMatching("domain",$_SESSION["pqcms-client-domain"]));
 echo "\n\n";
 var_dump($_SESSION);
 echo "\n\n";
 
 foreach(array_keys($_SESSION) as $sessionKey) {
-    if(str_starts_with($sessionKey,"electrocms-client"))
+    if(str_starts_with($sessionKey,"pqcms-client"))
         unset($_SESSION[$sessionKey]);
 }
 
 echo "\n\n";
 var_dump($_SESSION);
 echo "</pre>";
-//    unset($_SESSION["electrocms-client-logged"]);
-//    unset($_SESSION["electrocms-client-website-id"]);
-//    unset($_SESSION["electrocms-client-"]);
-//    header("location: ../");
-    die("Wylogowano. Niepoprawne przekierowanie.");
+
+header("location: ../");
+die("Wylogowano. Niepoprawne przekierowanie.");
