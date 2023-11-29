d4383ee083f5d84049db28b2685bc18cba9eea4e
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-29 23:00:50 +0100

    API, które pobiera userów ze strony (oraz filtruje)

---
+++ SERVER/api/website/hr/user/GetUser.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Content-type: application/json");
+
+require_once(dirname(__DIR__, 3) . "/utils/APIUtils.php");
+$response = APIUtils::validatePost($_POST,basename(__FILE__));
+if($response["suc"] == 0)
+    die(json_encode($response,JSON_UNESCAPED_UNICODE));
+
+$website = APIUtils::getWebsite($_POST);
+$users = $website->getUsers();
+
+$responseUsers = [];
+foreach($users as $user)
+{
+    if(isset($_POST["username"]) && $_POST["username"] !== $user["username"])
+        continue;
+    if(isset($_POST["nickname"]) && $_POST["nickname"] !== $user["nickname"])
+        continue;
+    if(isset($_POST["disabled"]) && $_POST["disabled"] !== $user["disabled"])
+        continue;
+    $responseUsers[] = $user;
+}
+
+echo json_encode(["suc" => 1, "resp" => $responseUsers],JSON_UNESCAPED_UNICODE);
