5929ff997a49cd3c6f70d7076718ee32e98dbe1d
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-29 22:57:40 +0100

    Poprawna returna API (wcześniejszy był bez "resp")

--- SERVER/api/website/auth/LogoutUser.php
+++ SERVER/api/website/auth/LogoutUser.php
@@ -7,7 +7,7 @@ $response = APIUtils::validatePost($_POST, basename(__FILE__));
 if($response["suc"] == 0) die(json_encode($response, JSON_UNESCAPED_UNICODE));
 
 if(empty($_POST["auth_key"]))
-    die(json_encode(["suc" => 0, "desc" => "Nie przesłano klucza uwierzytelniającego!", "post" => $_POST], JSON_UNESCAPED_UNICODE));
+    die(json_encode(["suc" => 1, "resp" => 0, "desc" => "Nie przesłano klucza uwierzytelniającego!"], JSON_UNESCAPED_UNICODE));
 
 require_once(dirname(__DIR__, 3) . "/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
