f546f3c225bc58286709c5e7497794b5682a6433
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-29 22:59:50 +0100

    API dodające użytkownika do strony

---
+++ SERVER/api/website/hr/user/AddUser.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Content-type: application/json");
+
+require_once(dirname(__DIR__, 3) . "/utils/APIUtils.php");
+$response = APIUtils::validatePost($_POST,basename(__FILE__));
+if($response["suc"] == 0)
+    die(json_encode($response,JSON_UNESCAPED_UNICODE));
+
+if(empty($_POST["username"]) || empty($_POST["nickname"]) || empty($_POST["password"]) || !isset($_POST["disabled"]))
+    die(json_encode(["suc" => 1, "resp" => 0, "desc" => "Uzupełnij wszystkie pola!"],JSON_UNESCAPED_UNICODE));
+
+$website = APIUtils::getWebsite($_POST);
+$errno = $website->addUser($_POST["username"], $_POST["nickname"], $_POST["password"], [], $_POST["disabled"]);
+echo json_encode(["suc" => 1, "resp" => $errno == 0, "desc" => $errno],JSON_UNESCAPED_UNICODE);
