fba73d105079ef2af1ef8d99ac94581e47ac2173
PoLeqq <wiktorsolinski123@gmail.com>
2023-11-30 08:01:14 +0100

    Poprawka czytelności kodu

--- SERVER/panel/login/login.php
+++ SERVER/panel/login/login.php
@@ -1,105 +1,110 @@
 <?php
 
-    header("Content-Type: application/json; charset=utf-8");
+header("Content-Type: application/json; charset=utf-8");
+session_start();
 
-    session_start();
+if(isset($_SESSION["pqcms-server-admin-logged"]) && $_SESSION["pqcms-server-admin-logged"])
+{
+    header("location: ../");
+    die("You are already logged in! If not redirected, try refreshing the page.");
+}
 
-    if(isset($_SESSION["pqcms-server-admin-logged"]) && $_SESSION["pqcms-server-admin-logged"]) {
-        header("location: ../");
-        die("You are already logged in! If not redirected, try refreshing the page.");
-    }
+if(!isset($_SESSION["pqcms-server-token-login"]) || !isset($_SESSION["pqcms-server-token-login-expire"]))
+    die("Wrong token. Reload the form.");
+if(time() >= $_SESSION["pqcms-server-token-login-expire"])
+    die("Token has expired. Reload the form.");
+if($_SESSION["pqcms-server-token-login"] != $_POST["token"])
+    die("Incorrect token.");
 
-    if(!isset($_SESSION["pqcms-server-token-login"]) || !isset($_SESSION["pqcms-server-token-login-expire"]))
-        die("Wrong token. Reload the form.");
-    if(time() >= $_SESSION["pqcms-server-token-login-expire"])
-        die("Token has expired. Reload the form.");
-    if($_SESSION["pqcms-server-token-login"] != $_POST["token"])
-        die("Incorrect token.");
+if(!isset($_POST["username"]) || !isset($_POST["password"]))
+    die("Check your posts.");
 
-    if(!isset($_POST["username"]) || !isset($_POST["password"]))
-        die("Check your posts.");
+require_once(dirname(__DIR__, 2) . "/database/Connection.inc.php");
+if(isBanned($_SERVER["REMOTE_ADDR"]))
+    die("Your IP has been banned!");
 
-    require_once(dirname(__DIR__, 2) . "/database/Connection.inc.php");
-    if(isBanned($_SERVER["REMOTE_ADDR"]))
-        die("Your IP has been banned!");
+function addLoginHistory($ip, $username, $password, $logged): void
+{
+    $conn = Connection::getConnection();
+    if(!$logged) $logged = "0";
+    date_default_timezone_set('Europe/Warsaw');
+    $now = date("Y-m-d H:i:s");
+    $conn->query("INSERT INTO login_history VALUES (null,'$ip','$username','$password','$now',$logged)");
+    $conn->close();
+}
 
-    function addLoginHistory($ip, $username, $password, $logged): void
+function getTries($ip): int
+{
+    $conn = Connection::getConnection();
+
+    date_default_timezone_set("Europe/Warsaw");
+    $date = date("Y-m-d");
+
+    $result = $conn->query("SELECT date FROM login_history WHERE ip='$ip' AND date LIKE '$date%' AND logged=0");
+    $res = 5 - $result->num_rows;
+    $conn->close();
+    return $res;
+}
+
+function isBanned($ip): bool
+{
+    if(getTries($ip) <= 0) return true;
+
+    $conn = Connection::getConnection();
+    $result = $conn->query("SELECT * FROM login_banned_ips WHERE ip='$ip'");
+    $banned = $result->num_rows >= 1;
+
+    $conn->close();
+
+    if(!$banned) $banned = getTries($ip) <= 0;
+    return $banned;
+}
+
+
+function login($ip, $username, $password): bool
+{
+    if(isBanned($ip)) return false;
+
+    if(!empty($username) || !empty($password))
     {
         $conn = Connection::getConnection();
-        if(!$logged) $logged = "0";
-        date_default_timezone_set('Europe/Warsaw');
-        $now = date("Y-m-d H:i:s");
-        $conn->query("INSERT INTO login_history VALUES (null,'$ip','$username','$password','$now',$logged)");
-        $conn->close();
-    }
-
-    function getTries($ip): int
-    {
-        $conn = Connection::getConnection();
-
-        date_default_timezone_set("Europe/Warsaw");
-        $date = date("Y-m-d");
-
-        $result = $conn->query("SELECT date FROM login_history WHERE ip='$ip' AND date LIKE '$date%' AND logged=0");
-        $res = 5 - $result->num_rows;
-        $conn->close();
-        return $res;
-    }
-
-    function isBanned($ip): bool
-    {
-        if(getTries($ip) <= 0) return true;
-
-        $conn = Connection::getConnection();
-        $result = $conn->query("SELECT * FROM login_banned_ips WHERE ip='$ip'");
-        $banned = $result->num_rows >= 1;
-
-        $conn->close();
-        
-        if(!$banned) $banned = getTries($ip) <= 0;
-        return $banned;
-    }
-
-
-    function login($ip, $username, $password): bool
-    {
-        if(isBanned($ip)) return false;
-
-        if(!empty($username) || !empty($password)) 
+        $result = $conn->query("SELECT id, password FROM users WHERE username = '$username'");
+        if(mysqli_num_rows($result) == 1)
         {
-            $conn = Connection::getConnection();
-            $result = $conn->query("SELECT id, password FROM users WHERE username = '$username'");
-            if(mysqli_num_rows($result) == 1) 
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($password, $row['password']))
             {
-                $row = mysqli_fetch_assoc($result);
-                if(password_verify($password, $row['password'])) 
-                {
-                    addLoginHistory($ip,$username,$password,true);
+                addLoginHistory($ip,$username,"",true);
 
-                    $_SESSION["pqcms-server-admin-id"] = $row["id"];
-                    unset($_SESSION["pqcms-server-token-login"]);
-                    unset($_SESSION["pqcms-server-token-login-expire"]);
+                $_SESSION["pqcms-server-admin-id"] = $row["id"];
+                unset($_SESSION["pqcms-server-token-login"]);
+                unset($_SESSION["pqcms-server-token-login-expire"]);
 
-                    $conn->close();
-                    return true;
-                } else {
-                    addLoginHistory($ip,$username,$password,false);
-                    $conn->close();
-                    return false;
-                }
-            } else {
+                $conn->close();
+                return true;
+            }
+            else
+            {
                 addLoginHistory($ip,$username,$password,false);
                 $conn->close();
                 return false;
             }
         }
-
-        return false;
+        else
+        {
+            addLoginHistory($ip,$username,$password,false);
+            $conn->close();
+            return false;
+        }
     }
 
-    if(login($_SERVER["REMOTE_ADDR"],$_POST["username"],$_POST["password"])) {
-        header("Location: ../");
-        die("If not redirected, try refreshing the page.");
-    } else {
-        die("Wrong username/password. Attepts left: ".getTries($_SERVER["REMOTE_ADDR"]));
-    }
+    return false;
+}
+
+if(login($_SERVER["REMOTE_ADDR"],$_POST["username"],$_POST["password"]))
+{
+    header("Location: ../");
+    die("Logged in! If not redirected, try refreshing the page.");
+}
+else
+    die("Wrong username/password. Attempts left: ".getTries($_SERVER["REMOTE_ADDR"]));
