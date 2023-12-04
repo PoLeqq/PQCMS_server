fef16941daa9e11cadd96ecea86211cfdfc5a89e
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-04 21:45:19 +0100

    Zmiana ElectroCMS -> PQCMS

--- SERVER/panel/login/index.php
+++ SERVER/panel/login/index.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ElectroCMSServer - Login</title>
+    <title>PQCMSServer - Login</title>
 
     <link rel="stylesheet" href="main.css">
 
