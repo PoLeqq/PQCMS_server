61a3d6d9ae54fc2dd6bb0471198b70a91772f0b0
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-05 20:43:04 +0100

    Poprawki ścieżek, linków

--- SERVER/initializer/error/index.php
+++ SERVER/initializer/error/index.php
@@ -1,10 +1,19 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["pqcms-initializer-error"])) {
+    header("location: ../../");
+    die("Niepoprawne przekierowanie.");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <title>PQCMS - Inicjator</title>
 
-    <link rel="icon" type="image/x-icon" href="../../../images/PQCMS.svg">
+    <link rel="icon" type="image/x-icon" href="../../images/PQCMS.svg">
 
     <link rel="stylesheet" href="../../../bs5/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../../default.css">
@@ -17,18 +26,13 @@
             <header class="mb-4">
                 <a id="main-link" class="navbar-brand px-3 text-white" style="font-size: 40px!important;" href="../../../">
                     PQCMS
-                    <img src="../../../images/PQCMS.svg" alt="logo">
+                    <img src="../../images/PQCMS.svg" alt="logo">
                 </a>
             </header>
 
             <?php
-                session_start();
-                if(!isset($_SESSION["pqcms-initializer-error"])) {
-                    header("location: ../../");
-                    die("Niepoprawne przekierowanie.");
-                }
-                echo $_SESSION["pqcms-initializer-error"];
-                unset($_SESSION["pqcms-initializer-error"]);
+            echo $_SESSION["pqcms-initializer-error"];
+            unset($_SESSION["pqcms-initializer-error"]);
             ?>
         </div>
     </div>
--- SERVER/initializer/success/index.php
+++ SERVER/initializer/success/index.php
@@ -1,3 +1,13 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["pqcms-initializer-success"])) {
+    header("location: ../../");
+    die("Niepoprawne przekierowanie.");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -7,40 +17,35 @@
     <link rel="icon" type="image/x-icon" href="../../../images/PQCMS.svg">
 
     <link rel="stylesheet" href="../../../bs5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../../default.css">
+    <link rel="stylesheet" href="../../default.css">
     <link rel="stylesheet" href="../index.css">
     <link rel="stylesheet" href="success.css">
 </head>
 <body>
-<div id="site-container" class="d-flex justify-content-center align-items-center text-center">
-    <div class="result">
-        <header class="mb-4">
-            <a id="main-link" class="navbar-brand px-3 text-white" style="font-size: 40px!important;" href="../../../">
-                PQCMS
-                <img src="../../../images/PQCMS.svg" alt="logo">
-            </a>
-        </header>
+    <div id="site-container" class="d-flex justify-content-center align-items-center text-center">
+        <div class="result">
+            <header class="mb-4">
+                <a id="main-link" class="navbar-brand px-3 text-white" style="font-size: 40px!important;" href="../../../">
+                    PQCMS
+                    <img src="../../images/PQCMS.svg" alt="logo">
+                </a>
+            </header>
 
-        <?php
-            session_start();
-            if(!isset($_SESSION["pqcms-initializer-success"])) {
-                header("location: ../../");
-                die("Niepoprawne przekierowanie.");
-            }
+            <?php
             echo $_SESSION["pqcms-initializer-success"];
             unset($_SESSION["pqcms-initializer-success"])
-        ?>
-        Sukces
-        <noscript>
-            <a href="http://localhost/pqcms/server/client/">Wykryto wyłączony JavaScript! Kliknij tutaj, aby przekierować.</a>
-        </noscript>
-    </div>
+            ?>
+            Sukces
+            <noscript>
+                <a href="https://poleq.pl/server/client/">Wykryto wyłączony JavaScript! Kliknij tutaj, aby przekierować.</a>
+            </noscript>
+        </div>
 
-    <script>
-        setTimeout(() => {
-             window.location.replace("http://localhost/pqcms/server/client/");
-        },3000);
-    </script>
-</div>
+        <script>
+            setTimeout(() => {
+                 window.location.replace("https://poleq.pl/server/client/");
+            },3000);
+        </script>
+    </div>
 </body>
 </html>
