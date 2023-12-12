3bc2694c4795e1fbe554ace793d87131aa608fca
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-12 23:03:28 +0100

    Dodanie APIka do sprawdzania, czy user ma odpowiednie permisje (na razie dla admina wszystko tak, dla usera wszystko nie)

---
+++ SERVER/api/website/perms/HasPermission.php
@@ -0,0 +1,18 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+require_once(dirname(__DIR__,2)."/utils/APIUtils.php");
+
+$response = APIUtils::validatePost($_POST,basename(__FILE__));
+if($response["suc"] == 0) die(json_encode($response,JSON_UNESCAPED_UNICODE));
+
+$validatorResponse = Validator::validate([$_POST["auth_key"],$_POST["perm"]],["s(128)","s"]);
+
+require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
+$website = APIUtils::getWebsite($_POST);
+
+$hasPermission = $website->hasPermission($_SERVER["REMOTE_ADDR"],$_POST["auth_key"]);
+$response["resp"] = $hasPermission["suc"];
+if(isset($hasPermission["desc"])) $response["desc"] = $hasPermission["desc"];
+
+die(json_encode($response,JSON_UNESCAPED_UNICODE));
