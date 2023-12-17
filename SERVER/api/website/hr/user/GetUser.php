48f84b9c1972e759f65bc84b6e6bba120ef81bc6
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-17 17:32:52 +0100

    Dodanie nowego kryterium - admin

--- SERVER/api/website/hr/user/GetUser.php
+++ SERVER/api/website/hr/user/GetUser.php
@@ -11,15 +11,36 @@ $website = APIUtils::getWebsite($_POST);
 $users = $website->getUsers();
 
 $responseUsers = [];
-foreach($users as $user)
+
+if(isset($_POST["admin"]) && $_POST["admin"] === 1)
 {
-    if(isset($_POST["username"]) && $_POST["username"] !== $user["username"])
-        continue;
-    if(isset($_POST["nickname"]) && $_POST["nickname"] !== $user["nickname"])
-        continue;
-    if(isset($_POST["disabled"]) && $_POST["disabled"] !== $user["disabled"])
-        continue;
-    $responseUsers[] = $user;
+    $admin = null;
+    foreach($users as $user)
+    {
+        if(!isset($user["disabled"]))
+        {
+            $admin = $user;
+            break;
+        }
+    }
+    $responseUsers[] = $admin;
 }
+else
+{
+    foreach($users as $user)
+    {
+        if(isset($_POST["admin"]) && $_POST["admin"] == 0 && !isset($user["disabled"]))
+            continue;
+        if(isset($_POST["username"]) && $_POST["username"] !== $user["username"])
+            continue;
+        if(isset($_POST["nickname"]) && $_POST["nickname"] !== $user["nickname"])
+            continue;
+        if(isset($_POST["disabled"]) && $_POST["disabled"] !== $user["disabled"])
+            continue;
+        $responseUsers[] = $user;
+    }
+}
+
+//if(isset($_POST["admin"]) && $_POST["admin"] === 0)
 
 echo json_encode(["suc" => 1, "resp" => $responseUsers],JSON_UNESCAPED_UNICODE);
