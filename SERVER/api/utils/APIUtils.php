2ccdb59f4a465e2b2a5c53b64e518ac81c57df62
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-18 23:33:29 +0100

    Nowa funkcja - validatePostForAuthKey - sprawdza post'a i weryfikuje, czy sesja użytkownika jest aktualna

--- SERVER/api/utils/APIUtils.php
+++ SERVER/api/utils/APIUtils.php
@@ -19,7 +19,6 @@ class APIUtils
         if(empty($post["domain"]) || empty($post["secure_key"]))
             return ["suc" => 0, "desc" => "Sprawdź poprawność post'ów!"];
 
-
         $fields = ["domain","secure_key"];
         $validatorResponse = Validator::validate([$post["domain"],$post["secure_key"]],["s","s(128)"]);
         if($validatorResponse["suc"] == 0)
@@ -40,6 +39,42 @@ class APIUtils
 //        Unieważnienie klucza
         $website->invalidateSecureKey($post["secure_key"],$apiName);
         
+        return ["suc" => 1];
+    }
+
+    /**
+     * Funkcja podobna do APIUtils::validatePost, jednak posiada również sprawdzenie poprawności auth_key
+     * Funkcja sprawdza, czy podana tablica posiada klucze: domain, secure_key (podstawowe dane API).
+     * Jeżeli test przejdzie pomyślnie, klucz licencyjny zostaje unieważniony.
+     * @param array $post tablica $_POST
+     * @return array odpowiedź: "suc": (0/1), dla 0 również "desc": "string: opis błędu"
+     */
+    public static function validatePostForAuthKey(array $post): array
+    {
+//        Jeżeli nie ma auth_key to GG
+        if(empty($post["auth_key"]))
+            return ["suc" => 0, "desc" => "Akcja niemożliwa. Nie podano \"auth_key\"!"];
+
+//        Sprawdzenie, czy auth_key to string(128)
+        $fields = ["auth_key"];
+        $validatorResponse = Validator::validate([$post["auth_key"]],["s(128)"]);
+        if($validatorResponse["suc"] == 0)
+            return ["suc" => 0, "desc" => "Walidacja nie powiodła się dla pola \"{$fields[$validatorResponse["element_index"]]}\""];
+//            return ["suc" => 0, "desc" => "Walidacja nie powiodła się dla pola \"{$fields[$validatorResponse["element_index"]]}\" (podano: ${post["auth_key"]}"];
+
+//        Sprawdzenie, czy klucz ma wartości tylko 0-9,a-f
+        $insecureCharsResponse = SQLSecurity::generateResponseForAPI(SQLSecurity::doesStringContains($post["auth_key"],SQLSecurity::getKeyCharacters(),true),"auth_key");
+        if(sizeof($insecureCharsResponse) !== 0)
+            return $insecureCharsResponse;
+
+//        Sprawdzenie, czy sesja jest dalej aktywna na serwerach PQCMS
+        $website = self::getWebsite($post);
+
+        require_once(dirname(__DIR__,2)."/objects/website/AuthKey.inc.php");
+        if(!AuthKey::isValidAuthKeyForIp($website->getId(),$_SERVER["REMOTE_ADDR"],$post["auth_key"])["valid"])
+            return ["suc" => 0, "desc" => "Sesja konta jest nieaktywna!"];
+
+//        Jest wszystko super :)
         return ["suc" => 1];
     }
 
