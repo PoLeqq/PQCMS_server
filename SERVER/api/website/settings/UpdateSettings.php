9399f6d2a7f87b633b53fb8095bd625ea23d3422
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-21 18:51:24 +0100

    Zmiana typu API jako required_logged, sprawdzenie, czy user ma permisje do zmiany danych oraz uwzględnienie tego w returnie API

--- SERVER/api/website/settings/UpdateSettings.php
+++ SERVER/api/website/settings/UpdateSettings.php
@@ -1,5 +1,4 @@
 <?php
-// WAŻNA NOTKA DO API
 
 header('Content-Type: application/json; charset=utf-8');
 require_once(dirname(__DIR__,2)."/utils/APIUtils.php");
@@ -12,23 +11,95 @@ if((!isset($_POST["login_count"]) && empty($_POST["login_count_reset"])) ||
     (!isset($_POST["token_lifespan"]) && empty($_POST["token_lifespan_reset"])))
     die(json_encode(["suc" => 0, "desc" => "Uzupełnij wszystkie pola!"],JSON_UNESCAPED_UNICODE));
 
+$authKeyValidate = APIUtils::validatePostForAuthKey($_POST);
+if($authKeyValidate["suc"] === 0)
+    die(json_encode($authKeyValidate,JSON_UNESCAPED_UNICODE));
 require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
 
-//if(!Validator::validate($_POST["auth_key"],["s(128)"])["suc"])
-//    die(json_encode(["suc" =>]))
+// Mozolna weryfikacja, bo wszystko ręcznie... Ale czy da się to zrobić automatycznie?
+
+$apiFields = ["login_count","login_count_reset",
+    "login_session_time","login_session_time_reset",
+    "token_lifespan","token_lifespan_reset"];
+$fields = [];
+$fieldValidators = ["i(0-100)","i(0-1)",
+    "i(0-3600)","i(0-1)",
+    "i(0-600)","i(0-1)"];
+
+$sendingFieldValidators = [];
+$i = 0;
+foreach ($apiFields as $apiField)
+{
+    if(isset($_POST[$apiField]))
+    {
+        $fields[] = $_POST[$apiField];
+        $sendingFieldValidators[] = $fieldValidators[$i];
+    }
+    else
+        unset($apiFields[$apiField]);
+    $i++;
+}
+
+$validatorResponse = Validator::validate($fields,$sendingFieldValidators);
+if($validatorResponse["suc"] == 0)
+    die(json_encode(["suc" => 0, "desc" => "Walidacja nie powiodła się dla pola \"{$apiFields[$validatorResponse["element_index"]]}\"", "dev_msg" => $validatorResponse["desc"]],JSON_UNESCAPED_UNICODE));
 
 require_once(dirname(__DIR__,3)."/objects/website/AuthKey.inc.php");
-AuthKey::isAdminAuthKey($website->getId(),$_POST["auth_key"]);
 $settings = $website->getSettings();
 
-if(!empty($_POST["login_count_reset"])) $settings->resetLoginAttempts();
-else $settings->setLoginAttempts($_POST["login_count"]);
+$responseChanged = [];
+// Jeśli user ma permisje, zmień podane dane
+{
+    if(!empty($_POST["login_count"]) || !empty($_POST["login_count_reset"]))
+    {
+        if($website->hasPermission($_SERVER["REMOTE_ADDR"], $_POST["auth_key"], "pqcms.settings.login_count.set"))
+        {
+            if(!empty($_POST["login_count_reset"]))
+                $settings->resetLoginAttempts();
+            else
+                $settings->setLoginAttempts($_POST["login_count"]);
+            $responseChanged["login_count"] = 1;
+        }
+        else
+            $responseChanged["login_count"] = 0;
+    }
 
-if(!empty($_POST["token_lifespan_reset"])) $settings->resetTokenLifespan();
-else $settings->setTokenLifespan($_POST["token_lifespan"]);
 
-if(!empty($_POST["login_session_time"])) $settings->resetLoginSessionTime();
-else $settings->setLoginSessionTime($_POST["login_session_time"]);
+    if(!empty($_POST["login_count"]) || !empty($_POST["login_count_reset"]))
+    {
+        if($website->hasPermission($_SERVER["REMOTE_ADDR"], $_POST["auth_key"], "pqcms.settings.login_session_time.set"))
+        {
+            if(!empty($_POST["login_session_time_reset"]))
+                $settings->resetLoginSessionTime();
+            else
+                $settings->setLoginSessionTime($_POST["login_session_time"]);
+            $responseChanged["login_session_time"] = 1;
+        }
+        else
+            $responseChanged["login_session_time"] = 0;
+    }
 
-die(json_encode(["suc" => 1, "desc" => "Zmieniono ustawienia strony!"],JSON_UNESCAPED_UNICODE));
+    if(!empty($_POST["token_lifespan"]) || !empty($_POST["token_lifespan_reset"]))
+    {
+        if($website->hasPermission($_SERVER["REMOTE_ADDR"], $_POST["auth_key"], "pqcms.settings.token_lifespan.set"))
+        {
+            if(!empty($_POST["token_lifespan_reset"]))
+                $settings->resetTokenLifespan();
+            else
+                $settings->setTokenLifespan($_POST["token_lifespan"]);
+            $responseChanged["token_lifespan"] = 1;
+        }
+        else
+            $responseChanged["token_lifespan"] = 0;
+    }
+
+
+//    if($website->hasPermission($_SERVER["REMOTE_ADDR"], $_POST["auth_key"], "pqcms.settings.token_lifespan.set"))
+//        if(!empty($_POST["token_lifespan_reset"]))
+//            $settings->resetTokenLifespan();
+//        else
+//            $settings->setTokenLifespan($_POST["token_lifespan"]);
+}
+
+die(json_encode(["suc" => 1, "desc" => "Zmieniono ustawienia strony!", "changed" => $responseChanged],JSON_UNESCAPED_UNICODE));
