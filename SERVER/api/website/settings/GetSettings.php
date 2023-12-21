d4cd6d84ffdf864f21300280ac8149ef2384847d
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-21 18:50:42 +0100

    Zmiana typu API jako required_logged

--- SERVER/api/website/settings/GetSettings.php
+++ SERVER/api/website/settings/GetSettings.php
@@ -3,7 +3,15 @@ header('Content-Type: application/json; charset=utf-8');
 require_once(dirname(__DIR__,2)."/utils/APIUtils.php");
 
 $response = APIUtils::validatePost($_POST,basename(__FILE__));
-if($response["suc"] == 0) die(json_encode($response,JSON_UNESCAPED_UNICODE));
+if($response["suc"] == 0)
+    die(json_encode($response,JSON_UNESCAPED_UNICODE));
+
+$authKeyValidate = APIUtils::validatePostForAuthKey($_POST);
+if($authKeyValidate["suc"] === 0)
+    die(json_encode($authKeyValidate,JSON_UNESCAPED_UNICODE));
+
+// todo czy ma permisje
+//if($)
 
 require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
