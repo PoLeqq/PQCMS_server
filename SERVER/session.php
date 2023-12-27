6317a2ee816ec42c5c8a987ba62b95e1cc752356
PoLeqq <wiktorsolinski123@gmail.com>
2023-12-27 21:50:32 +0100

    Super zwrócenie sesji

--- SERVER/session.php
+++ SERVER/session.php
@@ -2,4 +2,4 @@
 header("content-type: application/json");
 session_start();
 //session_destroy();
-echo json_encode($_SESSION,JSON_PRETTY_PRINT);
+echo json_encode($_SESSION,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
