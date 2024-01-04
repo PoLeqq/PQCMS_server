e9c6e8cf53ca399c7681e121384037603d591811
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-04 23:45:43 +0100

    Skrócenie returna API'ka

--- SERVER/api/website/hr/rank/GetRank.php
+++ SERVER/api/website/hr/rank/GetRank.php
@@ -6,9 +6,4 @@ require_once(dirname(__DIR__, 3) . "/utils/APIUtils.php");
 APIUtils::validatePostForAuthKey($_POST,basename(__FILE__));
 
 $website = APIUtils::getSafeWebsite($_POST);
-$ranks = $website->getRanks();
-if($ranks["suc"] == 0)
-    die(json_encode($ranks,JSON_UNESCAPED_UNICODE));
-
-$responseRanks = $ranks["resp"];
-echo json_encode(["suc" => 1, "resp" => $responseRanks],JSON_UNESCAPED_UNICODE);
+die(json_encode($website->getRanks(),JSON_UNESCAPED_UNICODE));
