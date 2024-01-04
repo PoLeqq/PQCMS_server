8b65e3284b3944f8e856d90b404eee2a6174e094
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-04 23:45:08 +0100

    Zmianka pod "validatePostForAuthKey"

--- SERVER/api/website/hr/user/AddUser.php
+++ SERVER/api/website/hr/user/AddUser.php
@@ -3,9 +3,7 @@
 header('Content-Type: application/json; charset=utf-8');
 
 require_once(dirname(__DIR__, 3) . "/utils/APIUtils.php");
-$response = APIUtils::validatePost($_POST,basename(__FILE__));
-if($response["suc"] == 0)
-    die(json_encode($response,JSON_UNESCAPED_UNICODE));
+APIUtils::validatePostForAuthKey($_POST,basename(__FILE__));
 
 if(empty($_POST["username"]) || empty($_POST["nickname"]) || empty($_POST["password"]) || !isset($_POST["disabled"]))
     die(json_encode(["suc" => 0, "desc" => "Uzupełnij wszystkie pola!"],JSON_UNESCAPED_UNICODE));
