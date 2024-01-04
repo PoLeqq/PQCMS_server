4fbe3e98b370ac5e3fc02628432579c68738ce60
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-04 23:36:29 +0100

    Pobieranie danych pętlą

--- SERVER/api/website/settings/GetSettings.php
+++ SERVER/api/website/settings/GetSettings.php
@@ -8,10 +8,23 @@ require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
 $settings = $website->getSettings();
 
-$response["resp"] = [
-    "login_attempts" => $settings->getLoginAttempts(),
-    "login_session_time" => $settings->getLoginSessionTime(),
-    "token_lifespan" => $settings->getTokenLifespan()
+$permsValues = [
+    "logincount" => "login_attempts",
+    "loginsessiontime" => "login_session_time",
+    "tokenlifespan" => "token_lifespan",
 ];
+
+$response["suc"] = 1;
+foreach($permsValues as $perm => $value)
+{
+    if($website->hasPermission($_SERVER["REMOTE_ADDR"],$_POST["auth_key"],"pqcms.settings.system.$perm"))
+        $response["resp"][$value] = $settings->unsafe_getField($value);
+}
+
+//$response["resp"] = [
+//    "login_attempts" => $settings->getLoginAttempts(),
+//    "login_session_time" => $settings->getLoginSessionTime(),
+//    "token_lifespan" => $settings->getTokenLifespan()
+//];
 
 die(json_encode($response,JSON_UNESCAPED_UNICODE));
