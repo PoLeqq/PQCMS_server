a330f1c0e3f5a87a7fffa6fee0cef12befec5ad3
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-06 23:27:19 +0100

    Nowy API'k - zwrócenie daty wygaśnięcia licencji (jeśli ma permisje pqcms.data.licenseexpiration)

---
+++ SERVER/api/website/data/GetLicenseExpiration.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once(dirname(__DIR__,2)."/utils/APIUtils.php");
+
+APIUtils::validatePostForAuthKey($_POST,basename(__FILE__));
+
+require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
+$website = APIUtils::getSafeWebsite($_POST);
+
+if()
+die(json_encode($website->getLicenseExpiration(),JSON_UNESCAPED_UNICODE));
