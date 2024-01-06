cd17edc577ad8c3739f6a6727e1906feddf2e589
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-06 23:25:18 +0100

    Skrócenie kodu pod validatePostForAuthKey

--- SERVER/api/system/version/GetServerVersion.php
+++ SERVER/api/system/version/GetServerVersion.php
@@ -21,9 +21,7 @@ header('Content-Type: application/json; charset=utf-8');
 // "dev-1.0.0"
 
 require_once(dirname(__DIR__, 2) . "/utils/APIUtils.php");
-$response = APIUtils::validatePost($_POST,basename(__FILE__));
-if($response["suc"] == 0)
-    die(json_encode($response,JSON_UNESCAPED_UNICODE));
+APIUtils::validatePostForAuthKey($_POST,basename(__FILE__));
 
 require_once("Version.inc.php");
 $response["version"] = Version::getVersion("server",!empty($_POST["complex"]));
