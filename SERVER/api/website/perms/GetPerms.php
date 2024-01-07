d9e717d7d928e9758c8f4a7128bef01e84637bbd
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-07 23:13:25 +0100

    Zmiana returna: perms na resp (jak wszędzie)

--- SERVER/api/website/perms/GetPerms.php
+++ SERVER/api/website/perms/GetPerms.php
@@ -6,4 +6,4 @@ require_once(dirname(__DIR__, 2) . "/utils/APIUtils.php");
 APIUtils::validatePost($_POST,basename(__FILE__));
 
 require_once(dirname(__DIR__,3)."/objects/website/WebsitePermissions.php");
-echo json_encode(["suc" => 1, "perms" => WebsitePermissions::getPermissionsDescriptions()],JSON_UNESCAPED_UNICODE);
+echo json_encode(["suc" => 1, "resp" => WebsitePermissions::getPermissionsDescriptions()],JSON_UNESCAPED_UNICODE);
