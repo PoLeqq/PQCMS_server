6f64be0074d04329b4c2ae80baad2867a541bfc5
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-07 23:17:51 +0100

    Dodanie funkcji validateParamsForUser

--- SERVER/objects/website/WebsiteUser.inc.php
+++ SERVER/objects/website/WebsiteUser.inc.php
@@ -59,18 +59,80 @@ class WebsiteUser
         return $result;
     }
 
-    public static function addUser(int $websiteId, string $username, string $nickname, string $password, array $perms = [], bool $disabled = false): array
+    private static function validateParamsForUser(string $username, string $nickname, string $password, array $perms): array
     {
-//        Walidacja długości
-        if(strlen($username) < 5 || strlen($username) > 30)
+        $betterUsername = preg_replace('/\s+/', '', $username);
+        if($betterUsername !== $username)
+            return ["suc" => 0, "desc" => "Nazwa użytkownika nie może posiadać białych znaków (np. spacji)!"];
+
+        $betterNickname = trim($nickname);
+
+        $betterPassword = preg_replace('/\s+/', '', $password);
+        if($betterPassword !== $password)
+            return ["suc" => 0, "desc" => "Hasło nie może posiadać białych znaków (np. spacji)!"];
+
+        if(strlen($betterUsername) < 5 || strlen($betterUsername) > 30)
             return ["suc" => 0, "desc" => "Login musi mieć od 5 do 30 znaków!"];
-        if(strlen($nickname) < 2 || strlen($nickname) > 30)
+        if(strlen($betterNickname) < 2 || strlen($betterNickname) > 30)
             return ["suc" => 0, "desc" => "Nazwa użytkownika musi mieć od 2 do 30 znaków!"];
-        if(strlen($password) < 8 || strlen($password) > 50)
+        if(strlen($betterPassword) < 8 || strlen($betterPassword) > 50)
             return ["suc" => 0, "desc" => "Hasło musi mieć od 8 do 30 znaków!"];
         require_once "WebsitePermissions.php";
         if(!WebsitePermissions::isProperPermsArray($perms))
             return ["suc" => 0, "desc" => "Podano niepoprawne permisje!"];
+        return ["suc" => 1];
+    }
+
+    public static function addUser(int $websiteId, string $username, string $nickname, string $password, array $perms = [], bool $disabled = false): array
+    {
+//        Walidacja długości
+        $paramsValidator = self::validateParamsForUser($username, $nickname, $password, $perms);
+        if($paramsValidator["suc"] == 0)
+            return $paramsValidator;
+
+//        Walidacja - anti SQL injection
+        require_once(dirname(__DIR__,2)."/utils/SQLSecurity.php");
+        $insecureCharsResponse = SQLSecurity::generateResponseForAPI(SQLSecurity::doesStringContains($username),"username");
+        if(sizeof($insecureCharsResponse) !== 0)
+            return $insecureCharsResponse;
+        $insecureCharsResponse = SQLSecurity::generateResponseForAPI(SQLSecurity::doesStringContains($nickname),"nickname");
+        if(sizeof($insecureCharsResponse) !== 0)
+            return $insecureCharsResponse;
+
+        $conn = Connection::getConnection();
+
+        $query = $conn->query("SELECT username FROM websites_admins WHERE website_id = $websiteId");
+        if($query->fetch_row()[0] !== $username)
+        {
+            $query = $conn->query("SELECT id FROM websites_users WHERE website_id = $websiteId AND username = '$username'");
+            if($query->num_rows == 0)
+            {
+                $disabled = (int) $disabled;
+                if(is_null($perms))
+                    $perms = [];
+                $perms = json_encode($perms);
+
+                $password = password_hash($password,PASSWORD_DEFAULT);
+
+                $conn->query("INSERT INTO websites_users VALUES (null,$websiteId,'$username','$nickname','$password','$perms',$disabled)");
+                if($conn->errno === 0) $resp = ["suc" => 1, "desc" => "Dodano użytkownika!"];
+                else $resp = ["suc" => 0, "desc" => "Błąd podczas dodawania użytkownika. Kod błędu: ".($conn->errno)."!"];
+                $conn->close();
+            }
+            else $resp = ["suc" => 0, "desc" => "Już istnieje użytkownik o takim loginie!"];
+        }
+        else $resp = ["suc" => 0, "desc" => "Już istnieje użytkownik o takim loginie (administrator)!"];
+
+        return $resp;
+    }
+
+    public static function editUser(int $websiteId, string $username, string $nickname, string $password, array $perms = [], bool $disabled = false): array
+    {
+//        Walidacja pól
+        $paramsValidator = self::validateParamsForUser($username, $nickname, $password, $perms);
+        if($paramsValidator["suc"] == 0)
+            return $paramsValidator;
+
 
 //        Walidacja - anti SQL injection
         require_once(dirname(__DIR__,2)."/utils/SQLSecurity.php");
