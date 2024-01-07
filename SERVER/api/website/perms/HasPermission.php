ab4d0d50fe4147cbc646a04cc534c7b80c8a3edc
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-07 23:13:49 +0100

    Zmiana nazwy pliku (dostosowanie)

--- SERVER/api/website/perms/HasPermission.php
+++ SERVER/api/website/perms/HasPermission.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "PermsCommonCodeAPI.php";
+require_once "PermsCommonCodeAPI.inc.php";
 require_once(dirname(__DIR__, 3) . "/objects/Website.inc.php");
 $website = APIUtils::getWebsite($_POST);
 die(json_encode($website->hasPermissions($_SERVER["REMOTE_ADDR"],$_POST["auth_key"],$_POST["perms"]),JSON_UNESCAPED_UNICODE));
