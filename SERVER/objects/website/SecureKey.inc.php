707a9f36c2be28f432fd8c54c837b78bdfde28ab
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-11 22:06:08 +0100

    zmiana logiki generateSecureKey: dodatkowy argument - ip podporządkowanie do zmienionej struktury tabeli

--- SERVER/objects/website/SecureKey.inc.php
+++ SERVER/objects/website/SecureKey.inc.php
@@ -3,15 +3,17 @@
 require_once(dirname(__DIR__,2)."/database/Connection.inc.php");
 class SecureKey
 {
-    public static function generateSecureKey(int $website_id): string
+    public static function generateSecureKey(int $website_id, string $remoteAddr): string
     {
         $conn = Connection::getConnection();
 
+        date_default_timezone_set('Europe/Warsaw');
+        $date = date("Y-m-d H:i:s", time() + 3600);
         do {
             $secureKey = bin2hex(random_bytes(64));
-        } while($conn->query("SELECT id FROM websites_secure_keys WHERE website_id = $website_id AND secure_key = '$secureKey' AND used_time IS NULL")->num_rows > 0);
+        } while($conn->query("SELECT id FROM websites_secure_keys WHERE website_id = $website_id AND secure_key = '$secureKey' AND expired_time >= '$date'")->num_rows > 0);
 
-        $conn->query("INSERT INTO websites_secure_keys (website_id, secure_key) VALUES ($website_id, '$secureKey')");
+        $conn->query("INSERT INTO websites_secure_keys (website_id, secure_key, ip, expired_time) VALUES ($website_id, '$secureKey', '$remoteAddr', '$date')");
         $conn->close();
 
         return $secureKey;
