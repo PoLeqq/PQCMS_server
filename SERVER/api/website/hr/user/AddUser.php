515df6bdb9c0e19edb5e9b6fbca9f8542eef331d
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-20 23:41:01 +0100

    / - dodanie do posta wymogu "email", return API'ka poprawa returna

--- SERVER/api/website/hr/user/AddUser.php
+++ SERVER/api/website/hr/user/AddUser.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json; charset=utf-8');
 require_once(dirname(__DIR__, 3) . "/utils/APIUtils.php");
 APIUtils::validatePostForAuthKey($_POST);
 
-if(empty($_POST["username"]) || empty($_POST["nickname"]) || empty($_POST["password"]) || !isset($_POST["disabled"]))
+if(empty($_POST["username"]) || empty($_POST["nickname"]) || !isset($_POST["email"]) || empty($_POST["password"]) || !isset($_POST["disabled"]))
     die(json_encode(["suc" => 0, "desc" => "Uzupełnij wszystkie pola!"],JSON_UNESCAPED_UNICODE));
 
 if(empty($_POST["perms"]))
@@ -18,13 +18,6 @@ $parsedPerms = WebsitePermissions::parsePostPermsArray($_POST["perms"]);
 if(is_null($parsedPerms))
     die(json_encode(["suc" => 0, "desc" => "Podane permisje nie są poprawne!"]));
 
-$website = APIUtils::getWebsite($_POST);
-foreach($parsedPerms as $perm => $value)
-    if(!$website->hasPermission($_SERVER["REMOTE_ADDR"],$_POST["auth_key"],$perm))
-        unset($parsedPerms[$perm]);
-
 $website = APIUtils::getSafeWebsite($_POST);
-$resp = $website->addUser($_POST["username"], $_POST["nickname"], $_POST["password"], $parsedPerms, $_POST["disabled"]);
-if($resp["suc"] === 1)
-    $resp["perms"] = $parsedPerms;
+$resp = $website->addUser($_POST["username"], $_POST["nickname"], $_POST["email"], $_POST["password"], $parsedPerms, $_POST["disabled"]);
 echo json_encode($resp,JSON_UNESCAPED_UNICODE);
