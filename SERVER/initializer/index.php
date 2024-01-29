9a0329b1c3a2e013cac10dadce5d8c128195450d
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-29 22:32:56 +0100

    / - bugfix (ip errors)

--- SERVER/initializer/index.php
+++ SERVER/initializer/index.php
@@ -5,7 +5,7 @@ session_start();
 unset($_SESSION["pqcms-initializer-error"]);
 unset($_SESSION["pqcms-initializer-success"]);
 
-if(!isset($_POST["pqcms-domain"]) || !isset($_POST["pqcms-username"]) || !isset($_POST["pqcms-license-key"]))
+if(!isset($_POST["pqcms-domain"]) || !isset($_POST["pqcms-username"]) || !isset($_POST["pqcms-license-key"]) || !isset($_POST["ip"]))
     error("Przesłano niepoprawne wartości. Skontaktuj się z administratorem PQCMS!");
 
 if(empty($_POST["pqcms-domain"]) || empty($_POST["pqcms-username"]) || empty($_POST["pqcms-license-key"]))
@@ -18,7 +18,8 @@ if(!preg_match('/^[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}$/', $_POST["pq
     error("Klucz licencyjny podany w nieprawidłowym formacie!");
 
 require_once(dirname(__DIR__) . "/api/website/license/LicenseChecker.inc.php");
-$response = checkLicense($_SERVER["REMOTE_ADDR"],$_SERVER["HTTP_REFERER"],$_POST["pqcms-domain"],$_POST["pqcms-username"],$_POST["pqcms-license-key"]);
+$response = checkLicense($_POST["ip"],$_SERVER["HTTP_REFERER"],$_POST["pqcms-domain"],$_POST["pqcms-username"],$_POST["pqcms-license-key"]);
+
 if($response["suc"] == 1)
 {
     $_SESSION["pqcms-client-domain"] = $_POST["pqcms-domain"];
@@ -27,7 +28,7 @@ if($response["suc"] == 1)
     $_SESSION["pqcms-client-logged"] = true;
 
     require_once(dirname(__DIR__) . "/objects/Website.inc.php");
-    $_SESSION["pqcms-client-website-id"] = Website::getWebsiteIDByMatching("domain",$_POST["pqcms-domain"]);
+    $_SESSION["pqcms-client-website-id"] = Website::getWebsiteIDByMatchingDomain("domain",$_POST["pqcms-domain"]);
 
     $_SESSION["pqcms-initializer-success"] = "Pomyślnie zalogowano! Za chwilę nastąpi przekierowanie...";
     header("location: success/");
@@ -38,6 +39,8 @@ else
     $sessionError = $response["desc"];
     if(array_key_exists("tries_left",$response)) $sessionError .= " Pozostało prób: ".$response["tries_left"];
 //        $_SESSION["initializer-error"] = "Podano błędne dane/licencja wygasła! Pozostało Ci ".$response["tries_left"]." prób.";
+//    var_dump($response);
+//    die();
     $_SESSION["pqcms-initializer-error"] = $sessionError;
     header("location: error/");
     die("Niepoprawne przekierowanie. ".$_SESSION["initializer-error"]);
