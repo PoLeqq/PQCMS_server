9c4b63f53f50a52e625d7ffee2ed7b7138669263
PoLeqq <wiktorsolinski123@gmail.com>
2024-01-31 22:06:08 +0100

    / - uzależnienie API'ka od APIUtils

--- SERVER/api/website/perms/HasPermission.php
+++ SERVER/api/website/perms/HasPermission.php
@@ -4,15 +4,13 @@ require_once "PermsCommonCodeAPI.inc.php";
 verifyPosts();
 
 require_once(dirname(__DIR__, 3) . "/objects/Website.inc.php");
-$website = APIUtils::getWebsite($_POST);
+$website = APIUtils::getWebsite($_SERVER["REMOTE_ADDR"],$_POST);
 
 if(!empty($_POST["username"]) && is_string($_POST["username"]))
 {
-    $safeWebsite = APIUtils::getSafeWebsite($_POST);
-    // todo zrobić to przez safe website
-    die(json_encode($website->hasPermissionsByUsername($_POST["username"],$_POST["perms"]),JSON_UNESCAPED_UNICODE));
+    $safeWebsite = APIUtils::getSafeWebsite($_SERVER["REMOTE_ADDR"],$_POST);
+    APIUtils::endAPIscript(basename(__FILE__, '.php'),$_POST,$website->hasPermissionsByUsername($_POST["username"],$_POST["perms"]));
 }
 
 $response = $website->hasPermissions($_POST["client_ip"],$_POST["auth_key"],$_POST["perms"]);
-APIUtils::logAPI($_POST,$response);
-die(json_encode($response,JSON_UNESCAPED_UNICODE));
+APIUtils::endAPIscript(basename(__FILE__, '.php'),$_POST,$response);
