fc16f5969fd6e4bdf0f43816c85f362b47e22494
PoLeqq <wiktorsolinski123@gmail.com>
2024-03-12 22:02:39 +0100

    + nowy API'k do sprawdzenia, czy administratora stać na kolejny miesiąc licencji

---
+++ SERVER/api/website/data/IsLicenseRenewable.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+require_once(dirname(__DIR__,2)."/utils/APIUtils.php");
+
+APIUtils::validatePostForAuthKey($_SERVER["REMOTE_ADDR"],basename(__FILE__, '.php'),$_POST);
+
+require_once(dirname(__DIR__,3)."/objects/Website.inc.php");
+$website = APIUtils::getSafeWebsite($_SERVER["REMOTE_ADDR"],$_POST);
+
+//pqcms.?.licenseexpiration
+//if()
+
+$response = $website->isLicenseRenewable();
+APIUtils::endAPIscript(basename(__FILE__, '.php'),$_POST,$response);
